6d7f60f5447626e1afc9432c169feeaa091c9917
GANDHI PARTH MAHENDRA <gandhiparth.02.2005@gmail.com>
2025-06-09 23:24:17 +0530

    first commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({children}) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [IsSeller, setIsSeller] = useState(false);
+
+    const value = {navigate, user, setUser, setIsSeller, IsSeller}
+    return <AppContext.Provider value={value} >
+            {children}
+            </AppContext.Provider>
+}
+
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
